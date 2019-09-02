df9fd84e866f865c6fb3a1290b466a3aca315c8e
Luisfuentech <luisfuentes.au@gmail.com>
2019-09-01 23:06:53 -0500

    ...

--- api/routes.js
+++ api/routes.js
@@ -4,13 +4,13 @@ const { apiController } = require("./index");
 app.get("/category/:name", apiController.getProductByCategory);
 app.get("/product/products", apiController.getProducts);
 app.get("/products-category", apiController.getProductsCat);
-app.get("/shopping/:id", apiController.getShopping);
+app.get("/shopping/cart/:id", apiController.getShopping);
 app.get("/product/search", apiController.searchProductbyWords);
 
 app.post("/category", apiController.addCategory);
 app.post("/product", apiController.addProduct);
-app.post("/shopping", apiController.addShopping);
 app.post("/shopping/buy", apiController.makePurchase);
+app.post("/shopping", apiController.addShopping);
 
 app.put("/product/:id", apiController.updateProduct);
 
