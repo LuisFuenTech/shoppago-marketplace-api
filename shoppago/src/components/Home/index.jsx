ee7c3f84ad6bd21b25aa30fd2e1b8f39c114a90c
Luisfuentech <luisfuentes.au@gmail.com>
2019-09-06 23:09:03 -0500

    Script for showing cardDeck

---
+++ shoppago/src/components/Home/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import CarouselHome from "./Carousel";
+import CardDeck from "./CardDeck";
+import Loading from "../Loading";
+
+class Home extends Component() {
+  render() {
+    const cards = [];
+    const { categories } = props;
+
+    for (let i = 0; i < categories.length; i += 3) {
+      cards.push(<CardDeck categories={categories.slice(i, i + 3)} />);
+    }
+
+    return categories ? (
+      <div align="center">
+        <ul className="">{}</ul>
+        {cards}
+        <CarouselHome />
+      </div>
+    ) : (
+      <Loading message={"Loading..."} />
+    );
+  }
+}
+
+export default Home;
